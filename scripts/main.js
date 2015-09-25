ce50aab2b72df0f8822ddf2667b02849e63f1d85
Maxime Caly <max.caly@gmail.com>
2015-09-25 08:07:58 +0200

    Added new styles and sections

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,11 @@ $(document).ready(function() {
     var once = 1
     if (annyang && once == 1) {
         var commands = {
-            'hello': greetings,
+            '*hello': greetings,
+            /* *bip hello *bip <-- This would allow to confirm if hello is blurred in a sentance*/
             'hi': greetings,
             'hey': greetings,
-            'open *term': redirect,
+            'open *term': redirect
         };
         annyang.addCommands(commands);
         annyang.start();
@@ -21,7 +22,8 @@ var timeOfDay = "Night";
 var greetings = function() {
     $("#greetings").animate({
         height: 500
-    }, 500, function() {
+    }, 800, function() {
+        $("#greetings h1").css("opacity","0.1");
         $("#typed-greetings").typed({
             strings: ["Hi! I'm Max, nice to meet you^2000 <br> I am a French Software Engineering student in love with video games and programming ! ^1000 <br>I am passionate with the Gaming Industry and I love playing video games.  ^1000<br> I'm actually a hardcore player of Starcraft 2 <br> I swear I go outside too... ^2000 sometimes... ^1000 <br>I've lived 6 years in the U.S.A in OHIO and have always wanted to come back after my studies. "],
             typeSpeed: 10,
