3f735d125740a61e65492ce8350566e092f9412b
Maxime Caly <max.caly@gmail.com>
2015-10-10 01:42:49 +0200

    New features

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     var once = 1
     if (annyang && once == 1) {
         var commands = {
-            '*hello': greetings,
+            'hello': greetings,
             /* *bip hello *bip <-- This would allow to confirm if hello is blurred in a sentance*/
             'hi': greetings,
             'hey': greetings,
@@ -11,8 +11,16 @@ $(document).ready(function() {
         annyang.addCommands(commands);
         annyang.start();
     }
+    setInterval(function() {
+        $.getJSON('https://api.twitch.tv/kraken/streams/kunamatataeu', function(data) {
+            /*If the stream object exists then stream is live*/
+            if (data.stream != null) {
+                $(".stream-circle").css("background-color", "#2aff00");
+                $("#stream-text").html("Stream Online");
+            }
+        });
+    }, 60000);
 
-    $("#userIP").text(userIP);
 });
 
 /*
@@ -41,22 +49,24 @@ var greetings = function() {
         $("#greetings h1").css("opacity", "0.1");
         $("#typed-greetings").typed({
             strings: ["Hi! I'm Max, nice to meet you!^500 <br> I am a French Software Engineering Student in love with technology, programming and video games.^500 <br>Usually,^500 when I have free time and nothing planned I will play something.^500 My favorite game is Starcraft 2 and I like it because it's a real time strategy game. I have to think and innovate to win. ^500 <br> I'm in my last year before graduating of 5 years of college and getting my Masters. ^500 <br> I lived 6 years in OHIO when I was a kid and I've always wanted to live in the USA. ^500 <br>I successfully found my internship in Boulder, Colorado.^500 It was amazing, I met extroadinary people and learned a lot both in technical and social aspects. ^500 <br> I had the chance to work in two different companies, DragonDev and Human Design. ^500<br> Both were different experiences and projects that gave me broader view of startups in technology and web development. ^500 <br> I really hope to be able to do my last internship at the same place because it was an A^500W^500E^500S^500O^500M^500E adventure and I can't wait to be there!"],
-            typeSpeed: 10,
+            typeSpeed: 0,
             backDelay: 500,
             loop: false,
             loopCount: false,
             showCursor: false,
             onStringTyped: function() {
                 setTimeout(function() {
-                    slideToSection("#experience", 500);
+                    slideToSection("#map", 500);
                 }, 2000);
 
             }
         });
+        x
     })
 }
 
 var redirect = function(term) {
+    console.log("hi");
     var url = "http://" + term.toLowerCase() + ".com";
     window.open(url, '_blank');
 }
@@ -78,7 +88,7 @@ var ddIcon = L.divIcon({
 L.mapbox.accessToken = 'pk.eyJ1IjoiZHJlYW1sZWQiLCJhIjoiY2lmOWZ3dXI4MDAxbXVlbHkzZjcwd2dqcyJ9.C7u55GKKjraqiEnkRu9-LA';
 // Create a map in the div #map
 var map = L.mapbox.map('map', 'dreamled.cif9fwukg001rt0lxgykig72r').setView([40.014984, -105.270546], 6);
-
+map.scrollWheelZoom.disable();
 //Human Design Marker
 var humanDesignM = L.marker([40.0177185, -105.2819596], {
     icon: hdIcon
@@ -94,9 +104,9 @@ var humandesignDiv = $("#humandesign");
 
 
 dragonDevM.on("click", function() {
-    slideToSection("#dragondev",500);
+    slideToSection("#dragondev", 500);
 });
 
 humanDesignM.on("click", function() {
-    slideToSection("#humandesign",500);
+    slideToSection("#humandesign", 500);
 });
