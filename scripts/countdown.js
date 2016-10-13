17d162a8014cce2d4091b8897b450b272cb48a69
Maxime Caly <max.caly@gmail.com>
2016-10-12 18:19:33 -0600

    Updated countdown

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -1,6 +1,7 @@
 var countdown = document.querySelector("#countdown")
 var timer = document.querySelector("#countdown .timer")
-
+var hourTimer = document.querySelector("#countdown .hour-timer")
+var dayTimer = document.querySelector("#countdown .day-timer")
 var colors = [
     "rgba(208,239,177,.50)",
     "rgba(179,216,156,.50)",
@@ -12,16 +13,27 @@ var colors = [
 var meetWithAnna = moment("2016 10 25", "YYYY MM DD").valueOf() / 1000
 var now = moment().valueOf() / 1000
 var timeLeft = Math.floor(meetWithAnna - now)
+var timeLeftHours = timeLeft / 3600
+var timeLeftDays = timeLeft / 86400
 
-setInterval(function() {
+function showTimeLeft() {
     now = moment().valueOf() / 1000
+
     timeLeft = Math.floor(meetWithAnna - now)
+    timeLeftHours = timeLeft / 3600
+    timeLeftDays = timeLeft / 86400
+
     timer.innerHTML = timeLeft.toLocaleString()
-}, 100)
+    hourTimer.innerHTML = timeLeftHours.toLocaleString()
+    dayTimer.innerHTML = timeLeftDays.toLocaleString()
+}
+
+showTimeLeft()
+setInterval(showTimeLeft, 1000)
 
 var randomColor = Math.floor(Math.random() * colors.length)
 
-setInterval(function() {
+setInterval(function () {
     randomColor = Math.floor(Math.random() * colors.length)
     countdown.style.backgroundColor = colors[randomColor]
 }, 5000)
