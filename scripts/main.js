9dc57b3344d598b73597ea4732ea345c29d1f875
Maxime Caly <max.caly@gmail.com>
2016-10-12 22:26:51 -0600

    Updated API call on twitch for stream

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var once = 1
     if (annyang && once == 1) {
         var commands = {
@@ -12,11 +12,20 @@ $(document).ready(function() {
         annyang.start();
     }
 
-    $.getJSON('https://api.twitch.tv/kraken/streams/kunamatataeu', function(data) {
-        /*If the stream object exists then stream is live*/
-        if (data.stream != null) {
-            $(".stream-circle").css("background-color", "#2aff00");
-            $("#stream-text").html("Stream Online");
+    $.ajax({
+        url: 'https://api.twitch.tv/kraken/streams/kunamatataeu',
+        type: 'GET',
+        dataType: 'json',
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('Client-Id', 'd70esgd3z7nrisyuznehtqp8l5a1qeu');
+        },
+        success: function (data) {
+            console.log(data)
+            /*If the stream object exists then stream is live*/
+            if (data.stream !== null) {
+                $(".stream-circle").css("background-color", "#2aff00");
+                $("#stream-text").html("Stream Online");
+            }
         }
     });
 });
@@ -25,7 +34,7 @@ $(document).ready(function() {
     The var section has to look like this : #section_name
     The var milliseconds takes an integer
 */
-var slideToSection = function(section, milliseconds) {
+var slideToSection = function (section, milliseconds) {
     $('html,body').animate({
         scrollTop: $(section).offset().top
     }, milliseconds);
@@ -35,15 +44,15 @@ var dayOfWeek = "Saturday";
 var dayOfWeekPast = "Sunday";
 var timeOfDay = "Night";
 
-$(".fa-microphone").click(function(event) {
+$(".fa-microphone").click(function (event) {
     greetings();
 });
 
 
-var greetings = function() {
+var greetings = function () {
     $("#greetings").animate({
         height: 800
-    }, 800, function() {
+    }, 800, function () {
         $("#greetings h1").css("opacity", "0.1");
         $("#typed-greetings").typed({
             strings: ["Hi! I'm Max, nice to meet you!^500 <br> I am a French Software Engineering Student in love with technology, programming and video games.^500 <br>Usually,^500 when I have free time and nothing planned I will play something.^500 My favorite game is Starcraft 2 and I like it because it's a real time strategy game. I have to think and innovate to win. ^500 <br> I'm in my last year before graduating of 5 years of college and getting my Masters. ^500 <br> I lived 6 years in OHIO when I was a kid and I've always wanted to live in the USA. ^500 <br>I successfully found my internship in Boulder, Colorado.^500 It was amazing, I met extraordinary people and learned a lot both in technical and social aspects. ^500 <br> I had the chance to work in two different companies, DragonDev and Human Design. ^500<br> Both were different experiences and projects that gave me broader view of startups in technology and web development. ^500 <br> I really hope to be able to do my last internship at the same place because it was an A^500W^500E^500S^500O^500M^500E adventure and I can't wait to be there!"],
@@ -52,8 +61,8 @@ var greetings = function() {
             loop: false,
             loopCount: false,
             showCursor: false,
-            onStringTyped: function() {
-                setTimeout(function() {
+            onStringTyped: function () {
+                setTimeout(function () {
                     slideToSection("#map", 500);
                 }, 2000);
 
@@ -63,7 +72,7 @@ var greetings = function() {
     })
 }
 
-var redirect = function(term) {
+var redirect = function (term) {
     console.log("hi");
     var url = "http://" + term.toLowerCase() + ".com";
     window.open(url, '_blank');
@@ -101,11 +110,11 @@ var dragondevDiv = $("#dragondev");
 var humandesignDiv = $("#humandesign");
 
 
-dragonDevM.on("click", function() {
+dragonDevM.on("click", function () {
     slideToSection("#dragondev", 500);
 });
 
-humanDesignM.on("click", function() {
+humanDesignM.on("click", function () {
     slideToSection("#humandesign", 500);
 });
 
@@ -194,7 +203,7 @@ function walk(direction) {
 
 
 
-$("body").keydown(function(event) {
+$("body").keydown(function (event) {
     //Right
     if (event.keyCode === 39) {
         walk('right');
